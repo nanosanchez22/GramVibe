5bdfb2e486a86e9996856b5d779efa44739d3d23
Agustin <agustin@192.168.1.5>
2024-11-07 15:39:20 -0300

    arreglos profile

--- GramVibe/src/Components/ProfileStats/ProfileStats.jsx
+++ GramVibe/src/Components/ProfileStats/ProfileStats.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import './ProfileStats.css';
 
-function ProfileStats({ postsCount, friendsCount, profilePic }) {
+function ProfileStats({ postsCount, friendsCount }) {
   return (
-    <div id='stats_container'>
-      <div>
-        <img src={profilePic} alt="Profile" />
+    <div id="stats_container">
+      <div className="stat-item">
+        <span className="stat-title">Posts</span>
+        <span className="stat-count">{postsCount}</span>
       </div>
-      <div>
-        <div>
-          <span>{postsCount}</span>
-          <p>Posts</p>
-        </div>
-        <div>
-          <span>{friendsCount}</span>
-          <p>Friends</p>
-        </div>
+      <div className="stat-item">
+        <span className="stat-title">Friends</span>
+        <span className="stat-count">{friendsCount}</span>
       </div>
     </div>
   );
